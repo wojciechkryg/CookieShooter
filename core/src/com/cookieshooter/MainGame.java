862fba40a7dac1a3bf31a4649ca0c8e9c7284aa7
Unknown <krygwojciech@gmail.com>
2017-03-09 22:12:19 +0100

    Added simple example and controller

--- core/src/com/cookieshooter/MainGame.java
+++ core/src/com/cookieshooter/MainGame.java
@@ -3,31 +3,110 @@ package com.cookieshooter;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.cookieshooter.controller.Controller;
 
 public class MainGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+    public final static float PPM = 100;
+    public static SpriteBatch batch;
+    OrthographicCamera cam;
+    World world;
+    Viewport viewport;
+    Box2DDebugRenderer b2dr;
+    Controller controller;
+    Body player;
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        cam = new OrthographicCamera();
+        world = new World(new Vector2(0, -10), true);
+        viewport = new FitViewport(Gdx.graphics.getWidth() / PPM, Gdx.graphics.getHeight() / PPM, cam);
+        b2dr = new Box2DDebugRenderer();
+        controller = new Controller();
+
+        createGround();
+        createPlayer();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        super.resize(width, height);
+        viewport.update(width, height);
+        controller.resize(width, height);
+    }
+
+    @Override
+    public void render() {
+        update(Gdx.graphics.getDeltaTime());
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.begin();
+        b2dr.render(world, cam.combined);
+        batch.end();
+
+        controller.draw();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        world.dispose();
+        b2dr.dispose();
+        controller.dispose();
+    }
+
+    private void update(float deltaTime) {
+        handleInput();
+        world.step(1 / 45f, 6, 2);
+        cam.position.set(viewport.getWorldWidth() / 2, viewport.getWorldHeight() / 2, 0);
+        cam.update();
+    }
+
+    private void handleInput() {
+        if (controller.isLeftPressed()) {
+            player.setLinearVelocity(new Vector2(-3, player.getLinearVelocity().y));
+        } else if (controller.isRightPressed()) {
+            player.setLinearVelocity(new Vector2(3, player.getLinearVelocity().y));
+        }
+    }
+
+    private void createGround() {
+        BodyDef bdef = new BodyDef();
+        bdef.position.set(viewport.getWorldWidth() / 2, 0);
+        bdef.type = BodyDef.BodyType.StaticBody;
+        Body b2body = world.createBody(bdef);
+
+        FixtureDef fdef = new FixtureDef();
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(viewport.getWorldWidth() / 2, 200 / PPM);
+
+        fdef.shape = shape;
+        b2body.createFixture(fdef);
+    }
+
+    private void createPlayer() {
+        BodyDef bdef = new BodyDef();
+        bdef.position.set(viewport.getWorldWidth() / 2, 80 / PPM);
+        bdef.type = BodyDef.BodyType.DynamicBody;
+        player = world.createBody(bdef);
+
+        FixtureDef fdef = new FixtureDef();
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(64 / PPM, 64 / PPM);
+
+        fdef.shape = shape;
+        player.createFixture(fdef);
+    }
 }
---
+++ core/src/com/cookieshooter/controller/Controller.java
@@ -0,0 +1,106 @@
+package com.cookieshooter.controller;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.cookieshooter.MainGame;
+
+public class Controller {
+
+    private enum ButtonKey {
+        LEFT, RIGHT;
+    }
+
+    private final static int ICON_SIZE = 128;
+    private final static int PADDING = 50;
+
+    Viewport viewport;
+    Stage stage;
+    boolean leftPressed, rightPressed;
+    OrthographicCamera cam;
+
+    public Controller() {
+        cam = new OrthographicCamera();
+        viewport = new FitViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), cam);
+        stage = new Stage(viewport, MainGame.batch);
+        Gdx.input.setInputProcessor(stage);
+
+        initTable();
+    }
+
+    private void initTable() {
+        Table table = new Table();
+        table.left().bottom();
+
+        Image leftImg = new Image(new Texture("left.png"));
+        leftImg.setSize(ICON_SIZE, ICON_SIZE);
+        leftImg.addListener(getOnButtonClickListener(ButtonKey.LEFT));
+
+        Image rightImg = new Image(new Texture("right.png"));
+        rightImg.setSize(ICON_SIZE, ICON_SIZE);
+        rightImg.addListener(getOnButtonClickListener(ButtonKey.RIGHT));
+
+        table.add(leftImg).size(leftImg.getWidth(), leftImg.getHeight());
+        table.add().size(PADDING, PADDING);
+        table.add(rightImg).size(rightImg.getWidth(), rightImg.getHeight());
+        table.pad(0, PADDING, PADDING, 0);
+
+        stage.addActor(table);
+    }
+
+    private InputListener getOnButtonClickListener(final ButtonKey btn) {
+        return new InputListener() {
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                switch (btn) {
+                    case LEFT:
+                        leftPressed = true;
+                        break;
+                    case RIGHT:
+                        rightPressed = true;
+                        break;
+                }
+                return true;
+            }
+
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                switch (btn) {
+                    case LEFT:
+                        leftPressed = false;
+                        break;
+                    case RIGHT:
+                        rightPressed = false;
+                        break;
+                }
+            }
+        };
+    }
+
+    public void draw() {
+        stage.draw();
+    }
+
+    public boolean isLeftPressed() {
+        return leftPressed;
+    }
+
+    public boolean isRightPressed() {
+        return rightPressed;
+    }
+
+    public void resize(int width, int height) {
+        viewport.update(width, height);
+    }
+
+    public void dispose() {
+        stage.dispose();
+    }
+}
