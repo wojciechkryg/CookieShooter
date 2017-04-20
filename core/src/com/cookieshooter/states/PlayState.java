e5de1e2980e617f37cdfdc872bb4a0242c1ee624
HannaH <hannahanslik@gmail.com>
2017-04-20 16:24:50 +0200

    Correct imports

--- core/src/com/cookieshooter/states/PlayState.java
+++ core/src/com/cookieshooter/states/PlayState.java
@@ -6,7 +6,6 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
-import com.badlogic.gdx.scenes.scene2d.ui.List;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.cookieshooter.objects.Bullet;
